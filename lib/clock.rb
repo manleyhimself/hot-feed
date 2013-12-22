2e8138ba6afbda061fc88af73007b38f343cbda1
manleyhimself <bcmanley1@gmail.com>
2013-12-22 02:00:13 -0500

    added lines from heroku exmp to clockwork.rb

--- lib/clock.rb
+++ lib/clock.rb
@@ -1,3 +1,8 @@
+require File.expand_path('../../config/boot',        __FILE__)
+require File.expand_path('../../config/environment', __FILE__)
+require 'clockwork'
+
+
 include Clockwork
 
 @feed = Feed.find(1)
